46ac2eed39eca9e4ac8b9a3743b359793d1e720a
jaBBQ <1929533730@qq.com>
2023-09-05 10:59:26 +0800

    runOnUIThread的使用

--- app/src/main/java/com/bignerdranch/android/android_test/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/android_test/MainActivity.kt
@@ -1,31 +1,10 @@
 package com.bignerdranch.android.android_test
 
-import android.os.AsyncTask
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-class MyAsyncTask(private val tv_content : TextView) : AsyncTask<Void,Void,String>() {
-    override fun onPreExecute() {
-        super.onPreExecute()
-        tv_content.text = "正在执行任务"
-    }
-    override fun doInBackground(vararg params: Void?): String {
-        try {
-            Thread.sleep(3000)
-        }catch (e : InterruptedException) {
-
-        }
-        return "执行完成"
-    }
-
-    override fun onPostExecute(result: String?) {
-        super.onPostExecute(result)
-        tv_content.text = result
-    }
-}
 
 class MainActivity : AppCompatActivity(){
     private var btn : Button? = null
@@ -34,9 +13,16 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
         btn = findViewById(R.id.btn)
         val tv_content = findViewById<TextView>(R.id.tv_content)
-        val myasyncTask = MyAsyncTask(tv_content)
+        val thread = Thread {
+            Thread.sleep(6000)
+            runOnUiThread {
+                tv_content.text = "已完成"
+            }
+        }
         btn?.setOnClickListener {
-            myasyncTask.execute()
+            Toast.makeText(this,"开始",Toast.LENGTH_SHORT).show()
+            thread.start()
+            Toast.makeText(this,"完成",Toast.LENGTH_SHORT).show()
         }
     }
 }
