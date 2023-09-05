18ff6e4792a44cf7d9eaa852ab3d2bff99d63cd4
jaBBQ <1929533730@qq.com>
2023-09-05 10:07:05 +0800

    AsyncTask的使用

--- app/src/main/java/com/bignerdranch/android/android_test/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/android_test/MainActivity.kt
@@ -1,55 +1,42 @@
 package com.bignerdranch.android.android_test
 
-import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+class MyAsyncTask(private val tv_content : TextView) : AsyncTask<Void,Void,String>() {
+    override fun onPreExecute() {
+        super.onPreExecute()
+        tv_content.text = "正在执行任务"
+    }
+    override fun doInBackground(vararg params: Void?): String {
+        try {
+            Thread.sleep(3000)
+        }catch (e : InterruptedException) {
+
+        }
+        return "执行完成"
+    }
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+        tv_content.text = result
+    }
+}
 
 class MainActivity : AppCompatActivity(){
     private var btn : Button? = null
-    var tv_content : TextView? = null
-    private var handler : Handler = object : Handler(Looper.getMainLooper()) {
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-            when(msg.what) {
-                0 -> {
-                    tv_content?.text = msg.obj.toString()
-                }
-            }
-            showToast()
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn = findViewById(R.id.btn)
-        tv_content = findViewById(R.id.tv_content)
+        val tv_content = findViewById<TextView>(R.id.tv_content)
+        val myasyncTask = MyAsyncTask(tv_content)
         btn?.setOnClickListener {
-            Thread(Runnable {
-                work()
-            }).start()
+            myasyncTask.execute()
         }
-    }
-
-    fun showToast() {
-        Toast.makeText(this,"done", Toast.LENGTH_SHORT).show()
-    }
-
-    fun work() {
-        var str = ""
-        for (i in 1..5) {
-            str += i.toString()
-        }
-        Thread.sleep(6000)
-        var msg : Message = Message()
-        msg.what = 0
-        msg.obj = str
-        handler.sendMessage(msg)
     }
 }
